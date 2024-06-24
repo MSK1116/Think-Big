ddd5708c7b444df8b7c68ce1aab4e1860a0de823
MSK1116 <anishkushwaha1116@gmail.com>
2024-06-24 10:34:03 +0545

    push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1> Thanks for finding us..! we will update this portal in future.</h1>
+      <h3>For more information: admin@thinkbig.org.np</h3>
+    </>
+  );
+};
+
+export default App;
