774206b45fd11d4308decf06b5bccd80e00667b0
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-03 20:32:03 +0545

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <>
       <h1 className="text-center "> Thanks for finding us..! we will update this portal in future.</h1>
-      <h3 className="text-center">For more information: admin@thinkbig.org.np</h3>
+      <h3 className="text-center">
+        For more information: admin@thinkbig.org.np or <a href="https://manishmahato.info.np">contact@msmahato.com.np</a>
+      </h3>
     </>
   );
 };
