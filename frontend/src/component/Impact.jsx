2601fb2552ca8d158237f35d216a15673fd47ac0
MANU0519 <anishkushwaga1116@gmail.com>
2024-08-28 18:01:12 +0545

    push

--- frontend/src/component/Impact.jsx
+++ frontend/src/component/Impact.jsx
@@ -7,28 +7,28 @@ const Impact = () => {
   const [counterState, setCounterState] = useState(false);
   return (
     <>
-      <div className="w-full p-11">
+      <div className="w-full md:p-11">
         <ScrollTrigger onEnter={() => setCounterState(true)} onExit={() => setCounterState(false)}>
-          <div className="   text-center  grid md:grid-cols-4 md:grid-rows-1 grid-cols-2 grid-rows-2 place-items-center ">
+          <div className="   text-center gap-y-10  grid md:grid-cols-4 md:grid-rows-1 grid-cols-2 grid-rows-2 place-items-center ">
             <div className="">
               <FaPeopleGroup className="m-auto w-16 h-16 text-gray-100" />
               <h1 className="text-5xl font-extrabold text-gray-50">{counterState && <CountUp start={50} end={100} duration={3} />}+</h1>
-              <p>Members</p>
+              <p className="text-sm text-gray-100">Members</p>
             </div>
             <div>
               <FaPeopleGroup className="m-auto w-16 h-16 text-gray-100" />
               <h1 className="text-5xl font-extrabold text-gray-50">{counterState && <CountUp start={10} end={50} duration={3} />}+</h1>
-              <p>Students Helped</p>
+              <p className="text-sm text-gray-100">Students Helped</p>
             </div>
             <div>
               <FaPeopleGroup className="m-auto w-16 h-16 text-gray-100" />
               <h1 className="text-5xl font-extrabold text-gray-50">{counterState && <CountUp start={50} end={90} duration={3} />}+</h1>
-              <p>Connected Cities</p>
+              <p className="text-sm text-gray-100">Connected Cities</p>
             </div>
             <div>
               <FaPeopleGroup className="m-auto w-16 h-16 text-gray-100" />
               <h1 className="text-5xl font-extrabold text-gray-50">{counterState && <CountUp start={50} end={100} duration={3} />}+</h1>
-              <p>Program conducted</p>
+              <p className="text-sm text-gray-100">Program conducted</p>
             </div>
           </div>
         </ScrollTrigger>
--- frontend/src/home/Home_banner2.jsx
+++ frontend/src/home/Home_banner2.jsx
@@ -10,13 +10,13 @@ const Home_banner2 = () => {
   return (
     <>
       <section className=" md:h-screen max-w-screen-2xl  bg-[url(/webi/school1.jpg)] bg-cover bg-top bg-no-repeat shadow-black rounded-md shadow-xl">
-        <div className="h-full  bg-gray-950/50 p-8  items-center flex flex-col space-y-11">
+        <div className="h-full  bg-gray-950/50 md:p-8 p-2 items-center flex flex-col space-y-11">
           <div data-aos="fade-up">
             <p data-aos="fade-up" className=" text-white text-3xl font-extrabold">
               We Learn, We Teach
             </p>
           </div>
-          <div data-aos="zoom-in-up" className="w-1/3">
+          <div data-aos="zoom-in-up" className="md:w-1/3 w-full">
             <p className="text-center text-gray-50 font-semibold">Passionate about discovering new knowledge and sharing it with others. Let’s inspire each other and make learning a joyful journey together! </p>
           </div>
           <Impact />
