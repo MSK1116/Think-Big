c9e53279d57efcd6c4aed16c8cbaef323b84196f
MANU0519 <anishkushwaga1116@gmail.com>
2024-08-29 07:40:27 +0545

    push

--- frontend/src/Faces/Faces1.jsx
+++ frontend/src/Faces/Faces1.jsx
@@ -10,7 +10,7 @@ const Faces1 = () => {
         <div className="text-center">
           <h1 className="text-3xl font-bold text-green-500">Visionary Thinkers</h1>
         </div>
-        <div className="grid grid-cols-4">
+        <div className="grid md:grid-cols-4 grid-cols-1">
           {Faces.map((Faces) => (
             <Faces1_card key={Faces.id} items={Faces} />
           ))}
