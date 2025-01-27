c5d3c1bcb89ed425a9e7186ef33aabe936e51544
MANU0519 <anishkushwaga1116@gmail.com>
2025-01-27 17:10:18 +0545

    fix: remove required validation from event model fields

--- backend/model/event.model.js
+++ backend/model/event.model.js
@@ -3,28 +3,21 @@ import mongoose from "mongoose";
 const eventSchema = mongoose.Schema({
   eventName: {
     type: String,
-    required: true,
   },
   Fullname: {
     type: String,
-    required: true,
-    unique: true,
   },
   email: {
     type: String,
-    required: true,
   },
   address: {
     type: String,
-    required: true,
   },
   textarea: {
     type: String,
-    required: true,
   },
   date: {
     type: String,
-    required: true,
   },
 });
 
