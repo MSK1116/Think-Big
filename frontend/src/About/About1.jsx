a0da79b7e6965d5e2f139d8be9748e0747fb447e
MANU0519 <anishkushwaga1116@gmail.com>
2025-01-31 09:42:28 +0545

    feat: update About section with new registration details and mission statement

--- frontend/src/About/About1.jsx
+++ frontend/src/About/About1.jsx
@@ -17,21 +17,26 @@ const About1 = () => {
 
         <div className="">
           <div className="relative p-0 h-screen overflow-hidden ">
-            <div className="absolute -bottom-8 inset-x-0 md:left-0 flex md:justify-start justify-center">
+            <div className="absolute -bottom-8  md:left-0 -right-20 flex md:justify-start justify-center">
               <SVGTree Hei={200} Wid={200} />
             </div>
 
             <div className=" hidden md:block absolute -right-44 ">
               <SVGNature Hei={800} Wid={800} />
             </div>
-            <div className="absolute left-6 top-28 w-full p-4 ">
+            <div className=" mt-28 p-4 px-8 w-full text-sm h-screen">
               <h1 className="text-3xl font-extrabold text-green-600">About us</h1>
-              <p className="text-red-500  text-sm"> Regd. No. 3435/080/081</p>
-              <p className="text-red-500  text-sm">We are registered under the Association Registration Act, 2035 BS, Nepal.</p>
-              <p className="text-red-500  text-sm">Complete detail about us and our legislation will be updated along with Social welfare council number by March 1.</p>
-              <p className=" mt-20 underline text-xl cursor-pointer" onClick={() => navigate("/event")}>
-                You can be clear about us through event page
-              </p>
+              <div className="md:w-1/2 p-1 md:p-2 w-[95%] h-full">
+                <h1 className=" mt-4 ">
+                  After tireless efforts by Bishnu P. Bhushal (Chief District Officer, Nepal Government) and our Organization President, Ram Julum Yadav, we finally got registration in 2024 with a mission to combat child exploitation, particularly
+                  child marriage and child labor, in Nepal’s remote areas. We are committed to empowering children through education, mentorship, and awareness programs, ensuring they have the knowledge and resources to break free from societal
+                  limitations and pursue brighter futures.
+                </h1>
+                <h1 className=" mt-6">
+                  Legal Status<br></br> Regd. No. 3435/080/081 We are registered under the Association Registration Act, 2035 BS, Nepal. Complete details about our organization and legal framework, including our Social Welfare Council number, will be
+                  updated by March 1, 2025. <br></br>🌱 Together, we can create a future where every child has the opportunity to learn, grow, and thrive.
+                </h1>
+              </div>
             </div>
           </div>
         </div>
