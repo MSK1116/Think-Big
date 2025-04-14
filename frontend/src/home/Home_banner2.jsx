5cf6665ee5c5f49c9534936d871dddb022d13201
MANU0519 <anishkushwaha1116@gmail.com>
2025-04-14 10:55:30 +0545

    feat: update title and headings for improved clarity and consistency in messaging

--- frontend/src/home/Home_banner2.jsx
+++ frontend/src/home/Home_banner2.jsx
@@ -12,12 +12,12 @@ const Home_banner2 = () => {
       <section className=" md:h-screen max-w-screen-2xl  bg-[url(/webi/school1.jpg)] bg-cover bg-top bg-no-repeat bg-gray-50 rounded-md ">
         <div className="h-full  bg-gray-950/50 md:p-8 p-2 items-center flex flex-col space-y-11">
           <div data-aos="fade-up">
-            <p data-aos="fade-up" className=" mt-6 text-white text-2xl text-center md:text-3xl font-extrabold">
+            <h1 data-aos="fade-up" className=" mt-6 text-white text-2xl text-center md:text-3xl font-extrabold">
               Together for Childhood, Against Injustice
-            </p>
+            </h1>
           </div>
           <div data-aos="zoom-in-up" className="md:w-1/3 w-full">
-            <p className="text-center text-gray-50 font-semibold">We are dedicated to raising awareness, ending child marriage and labor, and ensuring every child has the opportunity to grow, learn, and thrive in a safe environment. </p>
+            <h2 className="text-center text-gray-50 font-semibold">We are dedicated to raising awareness, ending child marriage and labor, and ensuring every child has the opportunity to grow, learn, and thrive in a safe environment. </h2>
           </div>
           <Impact />
         </div>
