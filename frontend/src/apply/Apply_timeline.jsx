790d996a04392ac2aadfe47a6b3e9b91904cda3e
MANU0519 <anishkushwaha1116@gmail.com>
2025-04-29 18:24:53 +0545

    fix: adjust layout and styling in Apply_timeline component for better responsiveness

--- frontend/src/apply/Apply_timeline.jsx
+++ frontend/src/apply/Apply_timeline.jsx
@@ -5,23 +5,23 @@ const Apply_timeline = () => {
     <>
       <div className=" p-8 mb-10 max-w-screen-2xl w-full h-full overflow-hidden">
         <h6 className=" text-xl font-semibold">Application timeline:</h6>
-        <ol className=" mt-9 relative flex flex-col md:flex-row gap-8 before:absolute before:-mt-px before:h-0.5 before:w-full before:rounded-full before:bg-gray-200">
+        <ol className=" mt-9 relative flex flex-col md:flex-row gap-8 md:before:absolute md:before:-mt-px md:before:h-0.5 md:before:w-full md:before:rounded-full md:before:bg-gray-200">
+          <div className="md:hidden absolute left-1 w-[2px] h-full bg-gray-200"></div>
           <li className="relative -mt-1.5">
             <span className="block size-3 rounded-full bg-emerald-400"></span>
-
-            <div className="mt-4">
+            <div className="mt-4 md:ml-0 ml-4">
               <time className="text-xs/none font-medium text-gray-700">April 29, 2025</time>
 
               <h3 className="text-lg font-bold text-gray-900 ">Application start</h3>
 
               <p className="mt-0.5 text-sm text-gray-700">Start preparing your answer and documents. Submit the above form and we will send you Link to application form.</p>
-            </div>
+            </div>{" "}
           </li>
 
           <li className="relative -mt-1.5">
             <span className="block size-3 rounded-full bg-emerald-500"></span>
 
-            <div className="mt-4">
+            <div className="mt-4 md:ml-0 ml-4">
               <time className="text-xs/none font-medium text-gray-700">May 20, 2025</time>
 
               <h3 className="text-lg font-bold text-gray-900">Application ends</h3>
@@ -33,7 +33,7 @@ const Apply_timeline = () => {
           <li className="relative -mt-1.5">
             <span className="block size-3 rounded-full bg-emerald-600"></span>
 
-            <div className="mt-4">
+            <div className="mt-4 md:ml-0 ml-4">
               <time className="text-xs/none font-medium text-gray-700">May 25, 2025</time>
 
               <h3 className="text-lg font-bold text-gray-900">Gossiping</h3>
@@ -44,7 +44,7 @@ const Apply_timeline = () => {
           <li className="relative -mt-1.5">
             <span className="block size-3 rounded-full bg-emerald-700"></span>
 
-            <div className="mt-4">
+            <div className="mt-4 md:ml-0 ml-4">
               <time className="text-xs/none font-medium text-gray-700">May 29, 2025</time>
 
               <h3 className="text-lg font-bold text-gray-900">Start studying</h3>
