ba05ed2043de30bb42f2af3bf2e51814b97e8a0e
MANU0519 <anishkushwaha1116@gmail.com>
2025-05-20 11:27:07 +0545

    fix: replace Image_Loader with img tag for logo rendering and ensure cursor visibility in ReactTyped

--- frontend/src/home/Home_banner.jsx
+++ frontend/src/home/Home_banner.jsx
@@ -22,10 +22,10 @@ const Home_banner = () => {
         <div className=" w-full h-full absolute inset-0 bg-[url(/webi/mountains.png)] bg-cover bg-center opacity-10 bg-no-repeat "></div>
         <div className="  w-full h-full bg-gradient-to-t z-10 from-gray-50 to-transparent flex flex-col items-center justify-center p-4 ">
           <div data-aos="zoom-out" data-aos-duration="1000" className="">
-            <Image_Loader styleForPar={" w-56 "} styleForImg={"w-full h-full "} src={Logo1} />
+            <img className=" w-56 " src={Logo1} alt="logo"></img>
           </div>
           <div data-aos="zoom-in-up" data-aos-duration="100" className=" text-center ">
-            <ReactTyped className="mt-0 font-playwrite bg-transparent text-green-600 font-extrabold text-xl  md:text-2xl" strings={[" Ending Exploitation, Ensuring Education"]} typeSpeed={40} />
+            <ReactTyped showCursor={false} className="mt-0 font-playwrite bg-transparent text-green-600 font-extrabold text-xl  md:text-2xl" strings={[" Ending Exploitation, Ensuring Education"]} typeSpeed={40} />
           </div>
         </div>
         <div></div>
